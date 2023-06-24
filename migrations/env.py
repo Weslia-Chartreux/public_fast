90e18bc46c5409101a48e1f46a8170898283d95f
Weslia <tstststt@yandex.ru>
2023-06-24 19:26:05 +0300

    migrations

---
+++ migrations/env.py
@@ -0,0 +1,21 @@
+import os
+from logging.config import fileConfig
+from os import environ
+from alembic import context
+from models import Address_table
+from dotenv import load_dotenv
+
+# Alembic Config объект предоставляет доступ
+# к переменным из файла alembic.ini
+load_dotenv('111.env')
+config = context.config
+
+section = config.config_ini_section
+config.set_section_option(section, "DB_USER", os.getenv("DB_USER"))
+config.set_section_option(section, "DB_PASS", os.getenv("DB_PASS"))
+config.set_section_option(section, "DB_NAME", os.getenv("DB_NAME"))
+config.set_section_option(section, "DB_HOST", os.getenv("DB_HOST"))
+
+fileConfig(config.config_file_name)
+
+target_metadata = [Address_table.metadata]
---
+++ migrations/versions/8b6273a4a979_added_required_tables.py
@@ -0,0 +1,24 @@
+"""Added required tables
+
+Revision ID: 8b6273a4a979
+Revises: 
+Create Date: 2023-06-24 19:24:34.108434
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8b6273a4a979'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
